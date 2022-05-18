a19b1579428093ec6f9980b4b136748f82d3a65e
Muhammad Syachrul <syachrulmuhammad@gmail.com>
2022-05-19 06:47:33 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Indonesia Areas
+
+In this repo, I was mapping data to JSON for personal usage.
+
+What was I do:
+
+- Execute script from `./src/wilayah.sql` in MySQL.
+- Export data from MySQL to `wilayah.json` and put in `src` directory.
+- Run `node index.js` to map and store the result to `data` directory.
+
+Source Data: https://github.com/cahyadsn/wilayah/blob/master/db/wilayah.sql
+
+Thank you to [cahyadsn](https://github.com/cahyadsn) for the data.
---
+++ index.js
@@ -0,0 +1,82 @@
+import fs from "fs/promises";
+
+const AREA_TYPE = {
+  PROVINCE: 2,
+  CITY: 5,
+  SUBDISTRICT: 8,
+  VILLAGE: 13,
+};
+
+const main = async () => {
+  const data = await getData();
+  build(data);
+
+  return 0;
+};
+
+const getData = async () => {
+  const file = await fs.readFile("./src/wilayah.json", "utf8");
+  return JSON.parse(file);
+};
+
+const build = (data) => {
+  const areas = data.reduce(
+    (acc, value) => {
+      switch (value.kode.length) {
+        case AREA_TYPE.PROVINCE:
+          acc.provinces.push({
+            code: value.kode,
+            name: value.wilayah,
+          });
+          break;
+
+        case AREA_TYPE.CITY:
+          acc.cities.push({
+            code: value.kode,
+            parent: value.kode.substring(0, AREA_TYPE.PROVINCE),
+            name: value.wilayah,
+          });
+          break;
+
+        case AREA_TYPE.SUBDISTRICT:
+          acc.subdistricts.push({
+            code: value.kode,
+            parent: value.kode.substring(0, AREA_TYPE.CITY),
+            name: value.wilayah,
+          });
+          break;
+
+        case AREA_TYPE.VILLAGE:
+          acc.villages.push({
+            code: value.kode,
+            parent: value.kode.substring(0, AREA_TYPE.SUBDISTRICT),
+            name: value.wilayah,
+          });
+          break;
+      }
+
+      return acc;
+    },
+    {
+      provinces: [],
+      cities: [],
+      subdistricts: [],
+      villages: [],
+    }
+  );
+
+  for (const area in areas) {
+    writeToFile(area, areas[area]);
+  }
+};
+
+const writeToFile = (fileName, data) => {
+  fs.writeFile(`./data/${fileName}-min.json`, JSON.stringify(data), {
+    flag: "w",
+  });
+  fs.writeFile(`./data/${fileName}.json`, JSON.stringify(data, null, 2), {
+    flag: "w",
+  });
+};
+
+main();
